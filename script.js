f38e10bde5c3772a30c2fbe877f8f6b7983e4e48
jcgomes08 <joaopedrogomes2023@icloud.com>
2024-08-11 19:13:27 -0300

    mudança

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function toggleMode() {
     // substituir a imagem
     if(html.classList.contains('light')) {
         //se tiver light mode, adicionar a imagem light
-        img.setAttribute('src', './assets/assets/avatar-light.png')
+        img.setAttribute('src', './assets/assets/pl.img.png')
         
     } else {
-    img.setAttribute('src', './assets/assets/avatar.png')
+    img.setAttribute('src', './assets/assets/pl.img.png')
     }
     
   
